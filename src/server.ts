701a8d0a03b9e26d694404fde2fcdc24e241f112
danieleb <dab@rob-ex.com>
2022-08-10 11:20:01 +0200

    Installed cors and mqtt. Uninstall express and setup of mqtt client. Removed old routes and app setup.

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,90 @@
-import app from './app';
+import mqtt from 'mqtt';
+import Device from './database/models/device';
+import Greenhouse from './database/models/greenhouse';
+import MeasurementLight from './database/models/measurements_light';
+import MeasurementTemperature from './database/models/measurements_temperature';
+import connection from './database/connection';
 
-app.listen(3333);
+const client = mqtt.connect('mqtt://192.168.1.2:1883', {
+	connectTimeout: 35000,
+});
+
+const temperatureTopic = 'temperature';
+const lightTopic = 'light';
+
+const temperatureCallback = (deviceId: string, message: string) => {
+	if (isNaN(Number(message))) return;
+
+	Device.findOne({ where: { hostname: deviceId } }).then((device) => {
+		if (device) {
+			Greenhouse.findOne({ where: { deviceId: (device as any).id } }).then(
+				(greenhouse) => {
+					if (greenhouse) {
+						MeasurementTemperature.create({
+							greenhouseId: (greenhouse as any).id,
+							temperature: Number(message),
+						});
+					}
+				},
+			);
+		}
+	});
+};
+
+const lightCallback = (deviceId: string, message: string) => {
+	if (isNaN(Number(message))) return;
+
+	Device.findOne({ where: { hostname: deviceId } }).then((device) => {
+		if (device) {
+			Greenhouse.findOne({ where: { deviceId: (device as any).id } }).then(
+				(greenhouse) => {
+					if (greenhouse) {
+						MeasurementLight.create({
+							greenhouseId: (greenhouse as any).id,
+							lightOn: message === '1' ? true : false,
+						});
+					}
+				},
+			);
+		}
+	});
+};
+
+interface Endpoint {
+	key: string;
+	topic: string;
+	callback: (deviceId: string, message: string) => void;
+}
+
+const endpoints: Endpoint[] = [
+	{
+		key: temperatureTopic,
+		topic: `${temperatureTopic}/+`,
+		callback: temperatureCallback,
+	},
+	{ key: lightTopic, topic: `${lightTopic}/+`, callback: lightCallback },
+];
+
+client.on('connect', async () => {
+	try {
+		await connection.authenticate();
+		console.log('Connection has been established successfully.');
+	} catch (error) {
+		console.log('Unable to connect to the database', error);
+	}
+});
+
+endpoints.forEach((endpoint) => {
+	console.log(`subscribing to ${endpoint.topic}`);
+	client.subscribe(endpoint.topic);
+});
+
+client.on('message', (topic, buffer) => {
+	const endpoint = endpoints.find((endpoint) => topic.startsWith(endpoint.key));
+
+	if (endpoint) {
+		const deviceId = topic.split('/')[1];
+		const message = buffer.toString();
+		endpoint.callback(deviceId, message);
+	}
+});
