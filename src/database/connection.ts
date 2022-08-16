5c9bf791d964e5d87b0c5b17129f1bf921569fe0
danieleb <dab@rob-ex.com>
2022-08-16 11:47:19 +0200

    Modified credentials with Sequelize. Created a Dockerfile.

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const connection = new Sequelize('greenhouse_db', 'root', 'dk25Pass', {
+const connection = new Sequelize('greenhouse_db', 'dk25', 'dk25Pass', {
 	host: '192.168.1.2',
 	dialect: 'mariadb',
 	port: 3306,
